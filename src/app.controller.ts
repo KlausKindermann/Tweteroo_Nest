8a235cb54e8053e5b00b76772f04dec4318cae82
klaus Kindermann <klauskindermann94@gmail.com>
2023-08-20 23:21:31 -0300

    pos0/tUsuario

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from './dtos/user.dto';
 
-@Controller()
+@Controller("users")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // Router.get("users", getUsers)
+  /*@Get()
+  getUsers() {
+    return this.appService.getUsers();
+  }
+
+  // function findOne(req: Request, res: Response) => { req.params.id  }
+  @Get(":id")
+  findOne(@Param("id", ParseIntPipe) id: number) {
+    console.log(id);
+    return id; // usuário correspondente ao id
+  }*/
+
+  // joi => name, email =? schema UserSchema ======= DTO (Data Transfer Object)
+  //function createUser(req: Request, res: Response) => { req.body as Omit<User, "id"> }
+  @Post()
+  createUser(@Body() body: CreateUserDto) {
+    return this.appService.createUser(body);
   }
 }
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/user.dto';
+import User from './entities/user.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private users: User[]; // persistencia em memoria
+
+  constructor() {
+      this.users.push(new User("ellen", "https://nerdhits.com.br/wp-content/uploads/2021/04/demon-slayer-kimetsu-no-yaiba-criador-time-100.jpg"));
+  }
+
+  createUser(userDTO: CreateUserDto) {
+    return this.users.push(userDTO.toUser());
   }
 }
